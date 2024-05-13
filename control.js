ad04b08c086c8168b0c823782d42f4dedc1f4382
Vicky-peace <victorpeace47@gmail.com>
2024-05-13 12:08:22 +0300

    operators

---
+++ control.js
@@ -0,0 +1,36 @@
+// Operators in  js
+// An operator is symbol that performs operations in one or more operands
+// 1. Arithmetiv=c operators
+
+
+let a = 10;
+let b = 10;
+
+console.log(` the value of {a} + {b} is ${a + b}`); //addition operator
+console.log(` the value of {a} % {b} is ${ a % b}`);//modulas operator
+console.log(` the value of {a} - {b} is ${a - b}`);//subtraction operator
+console.log(` the value of {a} * {b} is ${a * b}`); //multiplication operator
+console.log(` the value of {a} ** {b} is ${a ** b}`);// exponential operator
+console.log(` the value of {a}  is ${a++}`);//post-incremental operator
+console.log(` the value of {a} ++ is ${++a}`); //pre-increment operator
+
+
+
+
+
+// 2. Asssignemmet oparator
+let x = 10;
+let y = 20;
+let z =  15;
+
+x -=y;
+console.log(x);
+
+// Logical operators
+let x3 = 10;
+let y3 = 20;
+
+console.log(x3 == y3 && x3 < y3); //AND LOGICAL OPERATOR
+console.log(x3 == y3 || x3 < y3); //OR Operator
+console.log(!(x3 == y3)); //logical opertor
+
