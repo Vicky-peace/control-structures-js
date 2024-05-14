5d6ae54c019fdfd8e54c52aaf9e91e7ae56de0b2
Vicky-peace <victorpeace47@gmail.com>
2024-05-14 10:07:44 +0300

    maths methods

---
+++ maths.js
@@ -0,0 +1,24 @@
+// Maths in Js
+
+//1. Maths object
+console.log(Math.PI);
+
+
+//Maths methods
+//abs: returns the absolute value
+
+console.log(Math.abs(-5));
+
+//2. ceil: rounds to the nearest integer
+console.log(Math.ceil(10.41));
+
+//1. floor : rounds to the nearest integer returns the largest integer less than or equal to the number
+console.log(Math.floor(10.4));
+
+//3.round rounds to the nearest integer
+console.log(Math.round(10.2));
+
+
+//max and min: returns the highest (or lowest) value in a list of arguents
+console.log(Math.max(10,20,30,40,50));
+console.log(Math.min(10,20,30,40,50));
